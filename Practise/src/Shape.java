a96abd26145a97995571655b1b42ee3f4733128e
ravichandrapyda <63957358+ravichandrapyda@users.noreply.github.com>
2026-08-31 23:54:02 +0100

    "practising the interface class"

---
+++ Practise/src/Shape.java
@@ -0,0 +1,24 @@
+
+
+interface Shape{
+
+    void area();
+    void perimeter();
+
+}
+
+class  Rectangle implements Shape{
+
+
+    public void area(){
+
+        System.out.println(R*S);
+
+    }
+
+    public void perimeter(){
+
+
+
+    }
+}
